4d80e7b7beaf7e471e066a9ce3147a9144360f0e
Saverio Perrone <saverio.perrone20@gmail.com>
2024-08-27 18:53:09 +0200

    add input handler and routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,49 @@
+import { useState } from 'react';
 import websiteLogo from '../images/propLogo.png';
 import lensLogo from '../images/searchIcon.png';
+import '../styles/App.css'
+
+import { Link, useNavigate } from 'react-router-dom';
+
+function createTestData(numberOfElements){
+    const testData = [];
+    for (let i = 1; i <= numberOfElements; i++) {
+        const element = {};
+        element.name = `Prod${i}`;
+        element.description = `Prod${i} description`;
+        element.quantity = i;
+        element.key = `${i}`;
+        testData.push(element);
+    }
+    return testData;
+}
 
 function Header() {
+    const [input, setInput] = useState("");
+    const navigate = useNavigate()
+
     function handleSubmit(event) {
         event.preventDefault();
-        console.log('Input value:');
-      };
+        setInput('');
+        const testData = createTestData(30);
+        navigate('/products', {state: { products: testData, searchQuery: input} });
+    };
 
     return (
         <header>
-            <div className="logo">
-                <img src={websiteLogo} alt="Website Logo"/>
-            </div>
+            <Link to={'/'} className="logo">
+                <img src={websiteLogo} alt="Website Logo" />
+            </Link>
             <div className="search-bar">
                 <form onSubmit={handleSubmit}>
                     <div className='search-bar-form-div'>
-                        <input className='search-bar-input' type="text" placeholder="Cerca un prodotto"/>
+                        <input 
+                            className='search-bar-input' 
+                            type="text" 
+                            placeholder="Cerca un prodotto" 
+                            value={input} 
+                            onChange={(e) => setInput(e.target.value)} 
+                        />
                         <button className='search-bar-icon-button' type='submit'>
                             <img className='search-bar-icon' src={lensLogo} alt='Search button'></img>
                         </button>
