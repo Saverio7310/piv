86e63ab3c95b78f7cfeee79a0aae9ca068e9b97c
Saverio Perrone <saverio.perrone20@gmail.com>
2024-09-15 18:55:57 +0200

    add product and product prices classes

---
+++ src/model/Product.js
@@ -0,0 +1,95 @@
+import ProductPrices from "./ProductPrices";
+
+export default class Product {
+    #id = 0;
+    #name = '';
+    #description = '';
+    #image = '';
+    #prices = [];
+    #count = 1;
+
+    /**
+     * 
+     * @param {number} id 
+     * @param {string} name 
+     * @param {string} description 
+     * @param {string} image  
+     * @param {ProductPrices[]} prices 
+     */
+    constructor(id, name, description, image, prices) {
+        this.#id = id;
+        this.#name = name;
+        this.#description = description;
+        this.#image = image;
+        this.#prices = prices;
+    }
+
+    get getId() {
+        return this.#id;
+    };
+
+    get getName() {
+        return this.#name;
+    };
+
+    get getDescription() {
+        return this.#description;
+    };
+
+    get getPrices() {
+        return this.#prices;
+    };
+
+    get getImage() {
+        return this.#image;
+    };
+
+    get getCount() {
+        return this.#count;
+    }
+
+    set setId(id) {
+        this.#id = id;
+    };
+    
+    set setName(name) {
+        this.#name = name;
+    };
+
+    set setDescription(desc) {
+        this.#description = desc;
+    };
+
+    set setPrices(prices) {
+        this.#prices = prices;
+    };
+
+    set setImage(image) {
+        this.#prices = image;
+    };
+
+    set setCount(count) {
+        this.#count = count;
+    }
+
+    printProduct() {
+        return `Prod: ${this.#name}, ${this.#description}, ${this.#id}, ${this.#count}, ${this.#image}, ${this.#prices}`;
+    }
+
+    getProperties() {
+        return {
+            id: this.#id,
+            name: this.#name,
+            description: this.#description,
+            image: this.#image,
+        }
+    }
+
+    /* getPrice(key) {
+        return this.#prices.get(key);
+    }
+
+    setPrice(key, value) {
+        this.#prices.set(key, value);
+    } */
+}
---
+++ src/model/ProductPrices.js
@@ -0,0 +1,109 @@
+export default class ProductPrices {
+    #supermarketName = '';
+    #weigth = 0;
+    #unitPricesArray = [];
+    #pricesArray = [];
+    #discountedPrices = [];
+
+    /**
+     * 
+     * @param {string} supermarketName 
+     * @param {number} weigth 
+     * @param {number[]} unitPricesArray 
+     * @param {number[]} pricesArray 
+     * @param {number[]} discountedPrices 
+     */
+    constructor(supermarketName, weigth, unitPricesArray, pricesArray, discountedPrices) {
+        this.#supermarketName = supermarketName;
+        this.#weigth = weigth;
+        this.#unitPricesArray = unitPricesArray;
+        this.#pricesArray = pricesArray;
+        this.#discountedPrices = discountedPrices;
+    }
+
+    get getSupermarketName() {
+        return this.#supermarketName;
+    }
+
+    get getWeigth() {
+        return this.#weigth;
+    }
+    get getUnitPricesArray() {
+        return this.#unitPricesArray;
+    }
+    get getPricesArray() {
+        return this.#pricesArray;
+    }
+    get getDiscountedPrices() {
+        return this.#discountedPrices;
+    }
+
+    set setSupermarketName(supermarketName) {
+        this.#supermarketName = supermarketName;
+    }
+
+    set setWeigth(weigth) {
+        this.#weigth = weigth;
+    }
+
+    set setUnitPricesArray(unitPricesArray) {
+        this.#unitPricesArray = unitPricesArray;
+    }
+
+    set setPricesArray(pricesArray) {
+        this.#pricesArray = pricesArray;
+    }
+
+    set setDiscountedPrices(discountedPrices) {
+        this.#discountedPrices = discountedPrices;
+    }
+
+    getMinMax() {
+        let minValue = Number.MAX_SAFE_INTEGER;
+        let maxValue = Number.MIN_SAFE_INTEGER;
+        const len = this.#unitPricesArray.length;
+        for (let i = 0; i < len; i++) {
+            const el1 = this.#unitPricesArray[i];
+            const el2 = this.#pricesArray[i];
+            minValue = Math.min(minValue, el1, el2);
+            maxValue = Math.max(maxValue, el1, el2);
+        }
+        minValue = Math.max(0, Math.floor(minValue));
+        maxValue = Math.ceil(maxValue);
+        return { minValue, maxValue };
+    }
+
+    #getDiscountOccurrences() {
+        return this.#discountedPrices.reduce((acc, isDiscounted) => {
+            return acc + isDiscounted;
+        }, 0);
+    }
+
+    #getAVG(arrayToAVG, occ) {
+        return arrayToAVG.reduce((acc, price, index) => {
+            if (!this.#discountedPrices[index]) {
+                return acc + price
+            }
+            return acc + 0;
+        }, 0) / (arrayToAVG.length - occ);
+    }
+    
+    getAVGs() {
+        const occ = this.#getDiscountOccurrences();
+        const unitPricesAVG = this.#getAVG(this.#unitPricesArray, occ);
+        const pricesAVG = this.#getAVG(this.#pricesArray, occ);
+        return { unitPricesAVG, pricesAVG };
+    }
+
+    getLatestReport() {
+        const latestIndex = this.#unitPricesArray.length - 1;
+        return {
+            supermarketName: this.#supermarketName,
+            weigth: this.#weigth,
+            lastUnitPrice: this.#unitPricesArray[latestIndex],
+            lastPrice: this.#pricesArray[latestIndex],
+            nowDiscounted: this.#discountedPrices[latestIndex] ? true : false,
+        };
+    }
+
+}
