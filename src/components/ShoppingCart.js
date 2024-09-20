798d09478faa10b90385eccb951d9f93ccb3e593
Saverio Perrone <saverio.perrone20@gmail.com>
2024-09-20 18:01:10 +0200

    add remove button + logic and refactor cart optimizing function

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -2,9 +2,11 @@ import { useContext, useState } from "react";
 import { CartContext } from "./CartProvider";
 
 import '../styles/shoppingCart.css'
+import Product from "../model/Product";
+import { ImBin } from 'react-icons/im'
 
 function ShoppingCart() {
-    const { cart } = useContext(CartContext);
+    const { cart, handleRemoveProduct } = useContext(CartContext);
     const [selectedSupermarkets, setSelectedSupermarkets] = useState([]);
     const [optimizedProduct, setOptimizedProduct] = useState([]);
 
@@ -23,51 +25,57 @@ function ShoppingCart() {
         if (!found)
             newSelectedSupermarkets.push({ key: supermarketName, value: isChecked });
         setSelectedSupermarkets(newSelectedSupermarkets);
-        /* setSelectedSupermarkets((prevValues) => {
-            const values = [...prevValues];
-            const obj = values.find(pair => pair.key === supermarketName);
-            if (!obj)
-                values.push({ key: supermarketName, value: isChecked });
-            else
-                obj.value = isChecked;
-            return values;
-        }) */
     }
 
-    function handleOptimizationClick() {
-        const supermarketsSet = new Set(selectedSupermarkets.reduce((acc, { key, value }) => {
-            if (value)
-                return [...acc, key];
-            return acc;
-        }, []));
+    /**
+     * Returns a Set containing the selected supermarkets among the available ones
+     * @param {Object[]} selectedSupermarkets - Supermarkets array
+     * @param {string} selectedSupermarkets.key - Supermarket's name
+     * @param {boolean} selectedSupermarkets.value - Boolean value that points out the 
+     * selection of the user: true if it's been selected, false otherwise
+     * @returns {Set<string>}
+     */
+    function filterSelectedSupermarkets(selectedSupermarkets) {
+        return new Set(
+            selectedSupermarkets
+                .filter(({ _, value }) => value == true)
+                .map(({ key, _ }) => key)
+        );
+    }
 
-        console.log('Supermarket selezionati', supermarketsSet);
-
-        const product_supermarket_array = [];
-        cart.forEach((product) => {
-            const obj = {
-                productID: product.getId,
-                supermarket: '',
-                minPrice: Number.MAX_SAFE_INTEGER,
-                isDiscounted: false,
-            }
-            product.getPrices.forEach((price) => {
-                if (supermarketsSet.has(price.getSupermarketName)) {
-                    console.log('Seupermarket trovato');
-                    const latestPrice = price.getLatestPrice();
-                    if (latestPrice < obj.minPrice) {
-                        obj.supermarket = price.getSupermarketName;
-                        obj.minPrice = latestPrice;
-                        obj.isDiscounted = price.isNowDiscounted();
-                    }
-                } else {
-                    console.log('supermarket non trovato');
+    /**
+     * Returns, for the selected product, an object that contains the smallest price, its 
+     * related supermarket and whether it's actually discounted or not
+     * @param {Product} product - Poduct to check
+     * @param {Set<string>} supermarketsSet - Set of selected supermarkets
+     * @returns {Object} 
+     */
+    function findMinimumPriceForProduct(product, supermarketsSet) {
+        const productID = product.getId;
+        let supermarket = '';
+        let minPrice = Number.MAX_SAFE_INTEGER;
+        let isDiscounted = false;
+        product.getPrices.forEach((price) => {
+            if (supermarketsSet.has(price.getSupermarketName)) {
+                const latestPrice = price.getLatestPrice();
+                if (latestPrice < minPrice) {
+                    supermarket = price.getSupermarketName;
+                    minPrice = latestPrice;
+                    isDiscounted = price.isNowDiscounted();
                 }
-            });
-            product_supermarket_array.push(obj);
+            }
         });
-        console.log('product supermarket array', product_supermarket_array);
+        return { productID, supermarket, minPrice, isDiscounted };
+    }
 
+    /**
+     * Returns an array of objects. Each object contains the supermarket name and an
+     * array containig all the products that have the smallest price
+     * @param {Set<string>} supermarketsSet - Set of selected supermarkets
+     * @param {Object[]} product_supermarket_array - Array of products' info
+     * @returns {Object[]}
+     */
+    function findProductsBySupermarket(supermarketsSet, product_supermarket_array) {
         const finalArray = [];
         for (const supermarket of supermarketsSet.values()) {
             const arr = product_supermarket_array.filter((sup) => sup.supermarket === supermarket);
@@ -79,8 +87,37 @@ function ShoppingCart() {
             };
             finalArray.push(obj);
         }
+        
+        finalArray.sort((a, b) => a.supermarketName.localeCompare(b.supermarketName));
+        
+        return finalArray;
+    }
+
+    function handleShoppingCartOptimization() {
+        const supermarketsSet = filterSelectedSupermarkets(selectedSupermarkets);
+
+        const product_supermarket_array = cart.map((product) => {
+            return findMinimumPriceForProduct(product, supermarketsSet);
+        });
+
+        const finalArray = findProductsBySupermarket(supermarketsSet, product_supermarket_array);
+
         setOptimizedProduct(finalArray);
-        console.log('final array', finalArray);
+    }
+
+    function handleProductDeletion(prodID) {
+        handleRemoveProduct(prodID);
+        const optList = optimizedProduct;
+        optList.map(({ supermarketName, products }) => {
+            const prods = products.filter((prod) => prod.productID !== prodID);
+            if (prods.length === 0)
+                return null;
+            return {
+                supermarketName,
+                prods,
+            };
+        });
+        setOptimizedProduct(optList.filter(({ supermarketName, products }) => products.length !== 0));
     }
 
     if (cart.length === 0) {
@@ -105,9 +142,6 @@ function ShoppingCart() {
                                     <div className="product-list-item-element product-list-item-name">
                                         <h1 className="product-list-item-h1">{prod.getName}</h1>
                                     </div>
-                                    <div className="product-list-item-element product-list-item-quantity">
-                                        <p className="product-list-item-p">Quantità {prod.getCount}</p>
-                                    </div>
                                     <div className="product-list-item-element product-list-item-prices">
                                         {prod.getPrices.map((price, index) => {
                                             const { supermarketName, weight, lastUnitPrice, lastPrice, nowDiscounted } = price.getLatestReport();
@@ -116,12 +150,15 @@ function ShoppingCart() {
                                             );
                                         })}
                                     </div>
+                                    <div className="product-list-item-element product-list-item-quantity center-content">
+                                        <ImBin className="product-list-item-svg" onClick={() => handleProductDeletion(prod.getId)}></ImBin>
+                                    </div>
                                 </div>
                             </li>
                         );
                     })}
                 </ul>
-                <div style={{marginLeft: '10px'}}>
+                <div style={{ marginLeft: '10px' }}>
                     <p className="product-list-item-p">* I prezzi in sconto sono segnalati in arancione</p>
                 </div>
             </section>
@@ -147,7 +184,7 @@ function ShoppingCart() {
                             })}
                         </div>
                         <div className="center-content inner-spacing">
-                            <button className="main-function-button" onClick={handleOptimizationClick}>Ottimizza</button>
+                            <button className="main-function-button" onClick={handleShoppingCartOptimization}>Ottimizza</button>
                         </div>
                     </div>
                 </section>
