de165b423f370621ff992ca8b79f85c462d0a7c2
Saverio Perrone <saverio.perrone20@gmail.com>
2024-10-01 17:47:15 +0200

    add handler function to add selected product

--- src/components/SelectedProductProvider.js
+++ src/components/SelectedProductProvider.js
@@ -1,11 +1,18 @@
 import { createContext, useState } from "react";
+import SessionStorage from "../model/SessionStorage";
 
 export const SelectedProductContext = createContext();
 
 function SelectedProductProvider({children}) {
     const [selectedProduct, setSelectedProduct] = useState(null)
+
+    function handleAddSelectedProduct(product) {
+        setSelectedProduct(product);
+        SessionStorage.saveSelectedProduct(product);
+    }
+
     return (
-        <SelectedProductContext.Provider value={{selectedProduct, setSelectedProduct}}>
+        <SelectedProductContext.Provider value={{selectedProduct, handleAddSelectedProduct}}>
             {children}
         </SelectedProductContext.Provider>
     );
