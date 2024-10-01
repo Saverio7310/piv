6457c1e70225ebf8025f6e5795b8c41d2b269a0c
Saverio Perrone <saverio.perrone20@gmail.com>
2024-10-01 17:41:50 +0200

    update selected product loading from sessionStorage

--- src/components/ProductInfoPage.js
+++ src/components/ProductInfoPage.js
@@ -1,42 +1,24 @@
 import { useContext, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-
 import '../styles/productInfoPage.css'
 import ProductDiscountInfo from './ProductDiscountInfo';
-import { createTestDataWithPrices } from "../utils/generatePropData";
 import { PiShoppingCartThin } from 'react-icons/pi'
 import { CartContext } from "./CartProvider";
 import { SelectedProductContext } from "./SelectedProductProvider";
-import Product from "../model/Product";
 import { ToastContext } from "./ToastProvider";
+import SessionStorage from "../model/SessionStorage";
 
 function ProductInfoPage() {
     const { cart, handleAddProduct } = useContext(CartContext);
     const { addToast, TYPES } = useContext(ToastContext);
-    const { selectedProduct, setSelectedProduct } = useContext(SelectedProductContext);
-    const location = useLocation();
-    /**
-     * @type {Product}
-     */
-    const product = location.state?.product;
+    const { selectedProduct, handleAddSelectedProduct } = useContext(SelectedProductContext);
 
     useEffect(() => {
         console.log('Use Effect - Checking Product');
-        if (!product || !(product instanceof Product)) {
-            console.log('primo if');
-            if (selectedProduct && selectedProduct instanceof Product) {
-                console.log('secondo if');
-                //productRef.current = selectedProduct;
-            } else {
-                console.log('secondo else', selectedProduct, selectedProduct instanceof Product);
-                setSelectedProduct(undefined);
-            }
-        } else {
-            console.log('primo else');
-            product.setPrices = createTestDataWithPrices(3);
-            setSelectedProduct(product);
-        }
-    }, [setSelectedProduct]);
+        if (selectedProduct)
+            return;
+        const product = SessionStorage.getSelectedProduct();
+        handleAddSelectedProduct(product);
+    }, []);
 
     function handleAddProductToCart(selectedProduct) {
         const check = cart.findIndex((prod) => prod.getId === selectedProduct.getId);
