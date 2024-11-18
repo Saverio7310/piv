ba1ed9f8db1ec0aa58042f14fe493c0a709345e4
Saverio Perrone <saverio.perrone20@gmail.com>
2024-11-18 16:52:52 +0100

    add total price for each supermarket selected

--- src/components/ShoppingCartOptimizedList.js
+++ src/components/ShoppingCartOptimizedList.js
@@ -5,6 +5,12 @@ import ProductListItem from "./ProductListItem";
 
 function ShoppingCartOptimizedList({ supermarketName, products, handleProductCountChange }) {
     const { cart } = useContext(CartContext);
+   
+    const totalPrice = (products.reduce((acc, p) => {
+        const prod = cart.find((product) => product.getId === p.productID);
+        return acc += p.minPrice*prod.getCount;
+    }, 0)).toFixed(2);
+
     return (
         <section className="shopping-cart-section" key={`supermarket-section-${supermarketName}`}>
             <h1>{supermarketName}</h1>
@@ -15,6 +21,14 @@ function ShoppingCartOptimizedList({ supermarketName, products, handleProductCou
                         <ProductListItem type={0} prod={prod} minPrice={minPrice} isDiscounted={isDiscounted} handleProductCountChange={handleProductCountChange} />
                     );
                 })}
+                <div className="product-list-item shopping-cart-bill">
+                    <div className="product-list-item-element section-product-price shopping-cart-bill-dim">
+                        <h1 className="total-price product-list-item-price">Tot:</h1>
+                    </div>
+                    <div className="product-list-item-element section-product-price shopping-cart-bill-dim">
+                        <h1 className="total-price product-list-item-price">€{totalPrice}</h1>
+                    </div>
+                </div>
             </ul>
         </section>
     );
