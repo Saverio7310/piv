7c6abd268245a6e74880d7b583052665ff769e09
Saverio Perrone <saverio.perrone20@gmail.com>
2024-11-19 18:48:42 +0100

    move logic into separate file

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -6,6 +6,7 @@ import { ToastContext } from "./ToastProvider";
 import ShoppingCartProductsList from "./ShoppingCartProductsList";
 import ShoppingCartSupermarketsSelection from "./ShoppingCartSupermarketsSelection";
 import ShoppingCartOptimizedList from "./ShoppingCartOptimizedList";
+import shoppingCartOptimization from "../utils/optimizeShoppingCart";
 
 import Product from "../model/Product";
 import LocalStorage from "../model/LocalStorage";
@@ -22,7 +23,6 @@ function ShoppingCart() {
     useEffect(() => {
         const products = LocalStorage.getShoppingCart();
         console.log('UseEffect Shopping Cart - preloading');
-        console.log(products);
         if (cart.length === 0 && Array.isArray(products) && products.length !== 0)
             handleRestoreProducts(products);
     }, [cart.length, handleRestoreProducts]);
@@ -42,81 +42,8 @@ function ShoppingCart() {
         setSelectedSupermarkets(newSelectedSupermarkets);
     }
 
-    /**
-     * Returns a Set containing the selected supermarkets among the available ones
-     * @param {Object[]} selectedSupermarkets - Supermarkets array
-     * @param {string} selectedSupermarkets.key - Supermarket's name
-     * @param {boolean} selectedSupermarkets.value - Boolean value that points out the 
-     * selection of the user: true if it's been selected, false otherwise
-     * @returns {Set<string>}
-     */
-    function filterSelectedSupermarkets(selectedSupermarkets) {
-        return new Set(
-            selectedSupermarkets
-                .filter(({ _, value }) => value === true)
-                .map(({ key, _ }) => key)
-        );
-    }
-
-    /**
-     * Returns, for the selected product, an object that contains the smallest price, its 
-     * related supermarket and whether it's actually discounted or not
-     * @param {Product} product - Poduct to check
-     * @param {Set<string>} supermarketsSet - Set of selected supermarkets
-     * @returns {Object} 
-     */
-    function findMinimumPriceForProduct(product, supermarketsSet) {
-        const productID = product.getId;
-        let supermarket = '';
-        let minPrice = Number.MAX_SAFE_INTEGER;
-        let isDiscounted = false;
-        product.getPrices.forEach((price) => {
-            if (supermarketsSet.has(price.getSupermarketName)) {
-                const latestPrice = price.getLatestPrice();
-                if (latestPrice < minPrice) {
-                    supermarket = price.getSupermarketName;
-                    minPrice = latestPrice;
-                    isDiscounted = price.isNowDiscounted();
-                }
-            }
-        });
-        return { productID, supermarket, minPrice, isDiscounted };
-    }
-
-    /**
-     * Returns an array of objects. Each object contains the supermarket name and an
-     * array containig all the products that have the smallest price
-     * @param {Set<string>} supermarketsSet - Set of selected supermarkets
-     * @param {Object[]} product_supermarket_array - Array of products' info
-     * @returns {Object[]}
-     */
-    function findProductsBySupermarket(supermarketsSet, product_supermarket_array) {
-        const finalArray = [];
-        for (const supermarket of supermarketsSet.values()) {
-            const arr = product_supermarket_array.filter((sup) => sup.supermarket === supermarket);
-            if (arr.length === 0)
-                continue;
-            const obj = {
-                supermarketName: supermarket,
-                products: arr,
-            };
-            finalArray.push(obj);
-        }
-
-        finalArray.sort((a, b) => a.supermarketName.localeCompare(b.supermarketName));
-
-        return finalArray;
-    }
-
     function handleShoppingCartOptimization() {
-        const supermarketsSet = filterSelectedSupermarkets(selectedSupermarkets);
-
-        const product_supermarket_array = cart.map((product) => {
-            return findMinimumPriceForProduct(product, supermarketsSet);
-        });
-
-        const finalArray = findProductsBySupermarket(supermarketsSet, product_supermarket_array);
-
+        const finalArray = shoppingCartOptimization(selectedSupermarkets, cart);
         setOptimizedShoppingCart(finalArray);
     }
 
@@ -136,9 +63,6 @@ function ShoppingCart() {
         setOptimizedShoppingCart(optList);
         const id = Date.now();
         addToast({ id: id, type: TYPES.success, message: `Prodotto rimosso dal carrello` });
-        /* setSelectedSupermarkets([]);
-        setOptimizedShoppingCart([]);
-        handleRemoveProduct(prodID); */
     }
 
     function handleProductCountChange(prod, incrementValue) {
@@ -157,7 +81,7 @@ function ShoppingCart() {
         addToast({ id: id, type: TYPES.success, message: `Carrello svuotato` });
     }
 
-    function copyShoppingList() {
+    function handleShoppingListCopy() {
         let text = '';
         let total = 0;
         optimizedShoppingCart.forEach(({ supermarketName, products }) => {
@@ -207,7 +131,7 @@ function ShoppingCart() {
                     optimizedShoppingCart.length !== 0 &&
                     <div className="shopping-cart-info-container lateral-padding">
                         <div className="copy-button-container">
-                            <button className="copy-button primary-button" onClick={copyShoppingList}>Copia lista della spesa</button>
+                            <button className="copy-button primary-button" onClick={handleShoppingListCopy}>Copia lista della spesa</button>
                         </div>
                     </div>
                 }
---
+++ src/utils/optimizeShoppingCart.js
@@ -0,0 +1,75 @@
+/**
+     * Returns a Set containing the selected supermarkets among the available ones
+     * @param {Object[]} selectedSupermarkets - Supermarkets array
+     * @param {string} selectedSupermarkets.key - Supermarket's name
+     * @param {boolean} selectedSupermarkets.value - Boolean value that points out the 
+     * selection of the user: true if it's been selected, false otherwise
+     * @returns {Set<string>}
+     */
+function filterSelectedSupermarkets(selectedSupermarkets) {
+    return new Set(
+        selectedSupermarkets
+            .filter(({ _, value }) => value === true)
+            .map(({ key, _ }) => key)
+    );
+}
+
+/**
+ * Returns, for the selected product, an object that contains the smallest price, its 
+ * related supermarket and whether it's actually discounted or not
+ * @param {Product} product - Poduct to check
+ * @param {Set<string>} supermarketsSet - Set of selected supermarkets
+ * @returns {Object} 
+ */
+function findMinimumPriceForProduct(product, supermarketsSet) {
+    const productID = product.getId;
+    let supermarket = '';
+    let minPrice = Number.MAX_SAFE_INTEGER;
+    let isDiscounted = false;
+    product.getPrices.forEach((price) => {
+        if (supermarketsSet.has(price.getSupermarketName)) {
+            const latestPrice = price.getLatestPrice();
+            if (latestPrice < minPrice) {
+                supermarket = price.getSupermarketName;
+                minPrice = latestPrice;
+                isDiscounted = price.isNowDiscounted();
+            }
+        }
+    });
+    return { productID, supermarket, minPrice, isDiscounted };
+}
+
+/**
+ * Returns an array of objects. Each object contains the supermarket name and an
+ * array containig all the products that have the smallest price
+ * @param {Set<string>} supermarketsSet - Set of selected supermarkets
+ * @param {Object[]} product_supermarket_array - Array of products' info
+ * @returns {Object[]}
+ */
+function findProductsBySupermarket(supermarketsSet, product_supermarket_array) {
+    const finalArray = [];
+    for (const supermarket of supermarketsSet.values()) {
+        const arr = product_supermarket_array.filter((sup) => sup.supermarket === supermarket);
+        if (arr.length === 0)
+            continue;
+        const obj = {
+            supermarketName: supermarket,
+            products: arr,
+        };
+        finalArray.push(obj);
+    }
+
+    finalArray.sort((a, b) => a.supermarketName.localeCompare(b.supermarketName));
+
+    return finalArray;
+}
+
+export default function shoppingCartOptimization(selectedSupermarkets, cart) {
+    const supermarketsSet = filterSelectedSupermarkets(selectedSupermarkets);
+
+    const product_supermarket_array = cart.map((product) => {
+        return findMinimumPriceForProduct(product, supermarketsSet);
+    });
+
+    return findProductsBySupermarket(supermarketsSet, product_supermarket_array);
+}
