460f1f4fc0b709a467a1a30099f54de416ce5d20
Saverio Perrone <saverio.perrone20@gmail.com>
2025-01-28 16:05:48 +0100

    refactor ProductPrices class

--- src/model/ProductPrices.js
+++ src/model/ProductPrices.js
@@ -1,98 +1,104 @@
 export default class ProductPrices {
-    #supermarketName;
-    #weigth;
-    #unitPricesArray;
-    #pricesArray;
-    #discountedPrices;
-
     /**
      * 
      * @param {string} supermarketName 
      * @param {number} weigth 
-     * @param {number[]} unitPricesArray 
-     * @param {number[]} pricesArray 
+     * @param {number[]} unitPrices 
+     * @param {number[]} prices 
      * @param {number[]} discountedPrices 
+     * @param {number[]} discountedUnitPrices
+     * @param {string[]} dates
      */
-    constructor(supermarketName, weigth, unitPricesArray, pricesArray, discountedPrices) {
-        this.#supermarketName = supermarketName || '';
-        this.#weigth = weigth || 1;
-        this.#unitPricesArray = unitPricesArray || [];
-        this.#pricesArray = pricesArray || [];
-        this.#discountedPrices = discountedPrices || [];
+    constructor(supermarketName, weigth, unitPrices, prices, discountedPrices, discountedUnitPrices, dates) {
+        this.supermarketName = supermarketName || '';
+        this.weigth = weigth || 1;
+        this.unitPrices = unitPrices || [];
+        this.prices = prices || [];
+        this.discountedPrices = discountedPrices || [];
+        this.discountedUnitPrices = discountedUnitPrices || [];
+        this.dates = dates || [];
     }
 
     get getSupermarketName() {
-        return this.#supermarketName;
+        return this.supermarketName;
     }
 
     get getWeigth() {
-        return this.#weigth;
+        return this.weigth;
     }
-    get getUnitPricesArray() {
-        return this.#unitPricesArray;
+    get getUnitPrices() {
+        return this.unitPrices;
     }
-    get getPricesArray() {
-        return this.#pricesArray;
+    get getPrices() {
+        return this.prices;
     }
     get getDiscountedPrices() {
-        return this.#discountedPrices;
+        return this.discountedPrices;
+    }
+
+    get getDiscountedUnitPrices() {
+        return this.discountedUnitPrices;
+    }
+
+    get getDates() {
+        return this.dates;
     }
 
     set setSupermarketName(supermarketName) {
-        this.#supermarketName = supermarketName;
+        this.supermarketName = supermarketName;
     }
 
     set setWeigth(weigth) {
-        this.#weigth = weigth;
+        this.weigth = weigth;
     }
 
-    set setUnitPricesArray(unitPricesArray) {
-        this.#unitPricesArray = unitPricesArray;
+    set setUnitPrices(unitPrices) {
+        this.unitPrices = unitPrices;
     }
 
-    set setPricesArray(pricesArray) {
-        this.#pricesArray = pricesArray;
+    set setPrices(prices) {
+        this.prices = prices;
     }
 
     set setDiscountedPrices(discountedPrices) {
-        this.#discountedPrices = discountedPrices;
+        this.discountedPrices = discountedPrices;
     }
 
-    getProperties() {
-        return {
-            supermarketName: this.#supermarketName, 
-            weigth: this.#weigth,
-            unitPricesArray: this.#unitPricesArray,
-            pricesArray: this.#pricesArray,
-            discountedPrices: this.#discountedPrices,
-        }
+    set setDiscountedUnitPrices(discountedUnitPrices) {
+        this.discountedUnitPrices = discountedUnitPrices;
     }
 
-    static createInstance(obj) {
-        const pp = new ProductPrices();
-        const ppLiteral = pp.getProperties();
-        try {
-            for (const [ key, value ] of Object.entries(obj)) {
-                if (key in ppLiteral) {
-                    const setter = 'set' + key.charAt(0).toUpperCase() + key.slice(1);
-                    pp[setter] = value;
-                } else {
-                    throw new Error('Missing / Different property');
-                }
-            }
-        } catch (error) {
-            return null;
-        }
-        return pp;
+    set setDates(dates){
+        this.dates = dates;
+    }
+
+    addPrice(price) {
+        this.prices.push(price);
+    }
+
+    addDiscountedPrice(price) {
+        this.discountedPrices.push(price);
+    }
+
+    addUnitPrice(price) {
+        this.unitPrices.push(price);
+    }
+
+    addDiscountedUnitPrice(price) {
+        this.discountedUnitPrices.push(price);
+    }
+
+    addDate(date) {
+        this.dates.push(date);
     }
 
     getMinMax() {
         let minValue = Number.MAX_SAFE_INTEGER;
         let maxValue = Number.MIN_SAFE_INTEGER;
-        const len = this.#unitPricesArray.length;
+        const len = this.unitPrices.length;
         for (let i = 0; i < len; i++) {
-            const el1 = this.#unitPricesArray[i];
-            const el2 = this.#pricesArray[i];
+            const el1 = this.unitPrices[i];
+            const el2 = this.prices[i];
             minValue = Math.min(minValue, el1, el2);
             maxValue = Math.max(maxValue, el1, el2);
         }
@@ -101,47 +107,55 @@ export default class ProductPrices {
         return { minValue, maxValue };
     }
 
-    #getDiscountOccurrences() {
-        return this.#discountedPrices.reduce((acc, isDiscounted) => {
-            return acc + isDiscounted;
-        }, 0);
-    }
-
     #getAVG(arrayToAVG, occ) {
-        return arrayToAVG.reduce((acc, price, index) => {
-            if (!this.#discountedPrices[index]) {
-                return acc + price
-            }
-            return acc + 0;
-        }, 0) / (arrayToAVG.length - occ);
+        return arrayToAVG.reduce((acc, price) => {
+            return acc + price
+        }, 0) / (arrayToAVG.length);
     }
     
     getAVGs() {
-        const occ = this.#getDiscountOccurrences();
-        const unitPricesAVG = this.#getAVG(this.#unitPricesArray, occ);
-        const pricesAVG = this.#getAVG(this.#pricesArray, occ);
+        const unitPricesAVG = this.#getAVG(this.unitPrices);
+        const pricesAVG = this.#getAVG(this.prices);
         return { unitPricesAVG, pricesAVG };
     }
 
-    getLatestPrice() {
-        return this.#pricesArray[this.#pricesArray.length - 1];
+    getLatestPrice(nowDiscounted) {
+        if (nowDiscounted)
+            return this.discountedPrices[this.prices.length - 1];
+        return this.prices[this.prices.length - 1];
     }
 
-    getLatestUnitPrice() {
-        return this.#unitPricesArray[this.#unitPricesArray.length - 1];
+    getLatestUnitPrice(nowDiscounted) {
+        if (nowDiscounted)
+            return this.discountedUnitPrices[this.prices.length - 1];
+        return this.unitPrices[this.unitPrices.length - 1];
     }
 
     isNowDiscounted() {
-        return this.#discountedPrices[this.#discountedPrices.length - 1] === 1 ? true : false;
+        return this.discountedPrices[this.discountedPrices.length - 1] !== -1 ? true : false;
     }
     getLatestReport() {
+        const nowDiscounted = this.isNowDiscounted();
         return {
-            supermarketName: this.getSupermarketName,
-            weigth: this.getWeigth,
-            lastUnitPrice: this.getLatestUnitPrice(),
-            lastPrice: this.getLatestPrice(),
-            nowDiscounted: this.isNowDiscounted(),
+            nowDiscounted,
+            lastUnitPrice: this.getLatestUnitPrice(nowDiscounted),
+            lastPrice: this.getLatestPrice(nowDiscounted),
         };
     }
-
+    addProperty(key, value) {
+        const newKey = `set${key.at(0).toUpperCase()}${key.slice(1)}`;
+        this[newKey] = value;
+    }
+    static createInstance(obj) {
+        const prices = new ProductPrices();
+        try {
+            for (const [key, value] of Object.entries(obj)) {
+                prices.addProperty(key, value);
+            }
+            return prices;
+        } catch (error) {
+            console.error('Parsing error', error);
+        }
+        return prices;
+    }
 }
