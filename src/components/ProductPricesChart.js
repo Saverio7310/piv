7af8d20ed9dbd3473af6b8cb3c38ff3b7cccc7b4
Saverio Perrone <saverio.perrone20@gmail.com>
2025-01-28 15:53:03 +0100

    update data show logic

--- src/components/ProductPricesChart.js
+++ src/components/ProductPricesChart.js
@@ -20,42 +20,59 @@ ChartJS.register(
     Tooltip,
 );
 
-function ProductPricesChart({ prices }) {
-    const unitPricesArray = prices.getUnitPricesArray;
-    const pricesArray = prices.getPricesArray;
-    const discountedPrices = prices.getDiscountedPrices;
+function ProductPricesChart({ productPrice }) {
+    console.log('Product Prices Chart', productPrice);
+    const unitPrices = productPrice.getUnitPrices;
+    const prices = productPrice.getPrices;
+    const discountedPrices = productPrice.getDiscountedPrices;
+    const discountedUnitPrices = productPrice.getDiscountedUnitPrices;
+    const finalPrices = [];
+    const finalUnitPrices = [];
+    
+    for (let i = 0; i < prices.length; i++) {
+        if (discountedPrices[i] !== -1) {
+            finalPrices.push(discountedPrices[i]);
+            finalUnitPrices.push(discountedUnitPrices[i]);
+        } else {
+            finalPrices.push(prices[i]);
+            finalUnitPrices.push(unitPrices[i]);
+        }
+    }
+
+    const dates = productPrice.getDates.map(date => {
+        return date.split('-').reverse().join('/');
+    });
+    const colorMap  = discountedPrices.map((discountedPrice) => {
+        return discountedPrice !== -1 ? '#F6511D' : 'white';
+    })
     const data = {
-        labels: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', ].slice(0, unitPricesArray.length),
+        labels: dates,
         datasets: [
             {
-                label: 'Prezzi al Kg',
-                data: unitPricesArray,
+                label: 'Prezzi unitari',
+                data: finalUnitPrices,
                 backgroundColor: 'white',
                 borderColor: 'darkgrey',
                 pointBorderColor: 'darkgrey',
-                pointBackgroundColor: discountedPrices.map((isDiscounted) => {
-                    return isDiscounted ? '#F6511D' : 'white';
-                }),
+                pointBackgroundColor: colorMap,
                 borderWidth: 1.5,
                 pointRadius: 5,
                 pointHoverRadius: 8,
             },
             {
-                label: 'Prezzi prodotto',
-                data: pricesArray,
+                label: 'Prezzi finali',
+                data: finalPrices,
                 backgroundColor: 'white',
                 borderColor: 'black',
                 pointBorderColor: 'black',
-                pointBackgroundColor: discountedPrices.map((isDiscounted) => {
-                    return isDiscounted ? '#F6511D' : 'white';
-                }),
+                pointBackgroundColor: colorMap,
                 borderWidth: 1.5,
                 pointRadius: 5,
                 pointHoverRadius: 8,
             },
         ]
     };
-    const { minValue, maxValue } = prices.getMinMax();
+    const { minValue, maxValue } = productPrice.getMinMax();
     const options = {
         plugins: {
             legend: true
