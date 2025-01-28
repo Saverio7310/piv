66aac05d06366007e79e3612d21081de962e4ca5
Saverio Perrone <saverio.perrone20@gmail.com>
2025-01-28 15:50:48 +0100

    add prices fetch logic

--- src/components/ProductInfoPage.js
+++ src/components/ProductInfoPage.js
@@ -9,6 +9,8 @@ import { SelectedProductContext } from "./SelectedProductProvider";
 import SessionStorage from "../model/SessionStorage";
 
 import '../styles/ProductInfoPage.css';
+import ProductPrices from "../model/ProductPrices";
+import printCurrentInfo from "../utils/logObject";
 
 function ProductInfoPage() {
     const { cart, handleAddProduct } = useContext(CartContext);
@@ -16,12 +18,46 @@ function ProductInfoPage() {
     const { selectedProduct, handleAddSelectedProduct } = useContext(SelectedProductContext);
 
     useEffect(() => {
-        console.log('Use Effect - Checking Product');
+        console.log('Use Effect - Checking Product', printCurrentInfo(selectedProduct));
         if (selectedProduct)
             return;
         const product = SessionStorage.getSelectedProduct();
+        console.log('Preloaded product', printCurrentInfo(product));
         handleAddSelectedProduct(product);
     }, [handleAddSelectedProduct]);
+
+    useEffect(() => {
+        console.log('Use Effect - Fetching Prices', printCurrentInfo(selectedProduct));
+        if (selectedProduct && selectedProduct.getPrices.length !== 0) {
+            return;
+        }
+
+        async function fetchData() {
+            try {
+                const product_id = selectedProduct.getId;
+                const response = await fetch(`http://192.168.1.88:3030/api/v1/products/${product_id}/info`);
+                const serverResponseObject = await response.json();
+                const { rowCount, data } = serverResponseObject;
+                console.log('Fetched data (prices)', serverResponseObject);
+                const prices = new ProductPrices('Esselunga', 100);
+                data.map(obj => {
+                    prices.addDate(obj.created_at)
+                    prices.addPrice(obj.price)
+                    prices.addUnitPrice(obj.unit_price)
+                    prices.addDiscountedPrice(obj.discounted_price)
+                    prices.addDiscountedUnitPrice(obj.discounted_unit_price)
+                });
+                const p = selectedProduct.clone();
+                p.addPrice(prices);
+                handleAddSelectedProduct(p);
+            } catch (error) {
+                console.error('Error while fetching prices', error);
+            }
+        }
+        if (selectedProduct && selectedProduct.getPrices.length === 0) {
+            fetchData()
+        }
+    }, []);
 
     function handleAddProductToCart(selectedProduct) {
         const check = cart.findIndex((prod) => prod.getId === selectedProduct.getId);
@@ -34,8 +70,10 @@ function ProductInfoPage() {
             console.log('Prodotto già presente nel carrello');
         }
     }
-
-    if (!selectedProduct || !selectedProduct.getName) {
+    console.log(`
+    CHECKS:    
+    SELECTED PRODUCT:`, printCurrentInfo(selectedProduct));
+    if (!selectedProduct || !selectedProduct?.getName || selectedProduct.getPrices.length === 0) {
         return (
             <main>
                 <div>
@@ -52,7 +90,10 @@ function ProductInfoPage() {
                     <img src={selectedProduct.getImage} alt='Product' className='product-picture' />
                 </div>
                 <div className="product-info">
-                    <h1 className="product-title">{selectedProduct.getName}</h1>
+                    <h1 className="product-title">
+                        {selectedProduct.getName}
+                        <span className="product-description">{selectedProduct.getQuantityValue} {selectedProduct.getQuantityUnit}</span>
+                    </h1>
                     {/* <p>{selectedProduct.getDescription}</p> */}
                     <div className="add-cart">
                         <button className="add-cart-button primary-button" onClick={() => handleAddProductToCart(selectedProduct)}>
@@ -63,7 +104,7 @@ function ProductInfoPage() {
                         </button>
                     </div>
                 </div>
-                <ProductDiscountInfo product={selectedProduct.getPrices} />
+                <ProductDiscountInfo product={selectedProduct} />
             </div>
         </main>
     );
