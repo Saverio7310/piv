278015556d656a51387bc297bf0f4adc22f2d98d
Saverio Perrone <saverio.perrone20@gmail.com>
2025-02-10 16:46:30 +0100

    update landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,15 +1,22 @@
 import '../styles/LandingPage.css';
+import landscapeTitle from '../images/landing-page-title.png'
 
 function LandingPage() {
-    console.log('Landing page');
     const landingPageTitle = `Prezzi In Vista`;
-    const landingPageDesc = `Descrizione da definire`;
-    const landingPageHelp = `Maybe some images for reference`;
+    const landingPageDesc = `Fai la spesa consapevolmente`;
+    const landingPageHelp = `
+    Prezzi in Vista è il servizio che tiene traccia dei prezzi dei prodotti per te.
+    Controlla la veridicità degli sconti, confronta i prodotti e i prezzi tra supermercati.
+    `;
     return (
         <main>
             <div className='landing-page'>
-                <h1 className='landing-page-title'><span className='accent'>{landingPageTitle}</span></h1>
-                <h1 className='landing-page-desc'>{landingPageDesc}</h1>
+                <div className='title-image'>
+                    <img src={landscapeTitle} alt='Title' className='title-img' />
+                </div>
+                <div>
+                    <h1 className='landing-page-desc'>{landingPageDesc}</h1>
+                </div>
                 <p className='landing-page-help'>{landingPageHelp}</p>
             </div>
         </main>
